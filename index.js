bda507a9f75ddf784decf7a0e038764268ca18bf
Cebi-coder <cebisapeter40@gmail.com>
2020-10-01 17:28:01 +0200

    added new files

---
+++ index.js
@@ -0,0 +1,26 @@
+var plumbers = [
+    { business: plumbing, name: Avela, surname: Bhinqa },
+    { business: plumbing, name: Jacob, surname: Lubisi },
+    { business: plumbing, name: Isriel, surname: Sontonga },
+    { business: plumbing, name: Menzi, surname: Johnson },
+    { business: plumbing, name: Xolani, surname: Thimla },
+];
+
+var nails = [
+    { business: NailTech, name: Zinzi, surname: Nxele },
+    { business: NailTech, name: Bongiwe, surname: Zweni },
+    { business: NailTech, name: Bulelwa, surname: Mkhize },
+    { business: NailTech, name: Candice, surname: Mayers },
+    { business: NailTech, name: Julia, surname: Bidoli },
+    { business: NailTech, name: Hazel, surname: Peter },
+    { business: NailTech, name: Zenande, surname: Moya },
+];
+
+var movers = [
+
+    { business: mover, name: Senzo, surname: Sithole },
+    { business: mover, name: Mzwamadoda, surname: Sikhweyiya },
+    { business: mover, name: Mzwamadoda, surname: Sikhweyiya },
+    { business: mover, name: Mandla, surname: Sithole },
+
+];
